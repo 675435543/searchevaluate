b41b66dcd242771e2ef4ab0b1239bf0b850a2bbc
675435543 <675435543@qq.com>
2018-08-25 06:28:09 +0800

    基础框架

--- searchevaluate/src/main/java/com/test/controller/TestController.java
+++ searchevaluate/src/main/java/com/test/controller/TestController.java
@@ -3,6 +3,7 @@ package com.test.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
@@ -10,5 +11,11 @@ public class TestController {
 	@RequestMapping(value="/test", method=RequestMethod.GET)
 	private String test() throws Exception {
 		return "index";
+	}
+	
+	@RequestMapping(value="/test2", method=RequestMethod.GET)
+	@ResponseBody
+	private String test2() throws Exception {
+		return "haha";
 	}
 }
